234d3c85fd1b52841fdad1c68687924c67539571
OgerFurkan <furkan.ogr@icloud.com>
2025-10-20 08:52:25 +0300

    1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 				</a>
 			</div>
 			<h1>Manuel Deploy Projesi!</h1>
-			<p>1.0.0</p>
+			<p>1.0.1</p>
 			<div className="card">
 				<button type="button" onClick={() => setCount((count) => count + 1)}>
 					count is {count}
