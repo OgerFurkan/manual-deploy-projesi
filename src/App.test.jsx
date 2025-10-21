5e10b91b7ab0ce7cca3cdbb122cb6df05417f5a7
OgerFurkan <furkan.ogr@icloud.com>
2025-10-21 11:47:41 +0300

    add import userEvent

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 test("check header", () => {
 	render(<App />);
